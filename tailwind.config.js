b459f2972be9159e5058aefb6613d991bc252cbc
rinkesh375 <rinkesh@epesign.com>
2024-04-03 18:48:50 +0530

    done with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,7 @@
 module.exports = {
   content: ["*"],
   theme: {
-    screens: {
-      'xs': '320px', // Custom breakpoint for extra small screens paste in the screens object this will overrid the default break and it should only be written here only in the theme object
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-    extend: {
-      fontSize: {
-          "11xl":['25rem', { lineHeight: '2' }],
-        // Add more custom font sizes as needed
-      },
-      borderWidth: {
-       25:"25px"
-        
-      },
-      colors: {
-        customBorder: {
-          "red":"#33B3FF"
-        },
-      }
-      
-    },
+    
   },
   plugins: [],
 }
