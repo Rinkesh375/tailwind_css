bfee4baadae59a9d7d76ad569ee9b2cabc83148a
rinkesh375 <rinkesh@epesign.com>
2024-04-03 15:13:08 +0530

    adding addition classes using the extend and theme tailwind css classess

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,29 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'xs': '320px', // Custom breakpoint for extra small screens paste in the screens object this will overrid the default break and it should only be written here only in the theme object
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
-
-      // screens: {
-      //   'xs': '320px', // Custom breakpoint for extra small screens paste in the extend object
-      //   'sm': '640px',
-      //   'md': '768px',
-      //   'lg': '1024px',
-      //   'xl': '1280px',
-      //   '2xl': '1536px',
-      // },
+      fontSize: {
+          "11xl":['25rem', { lineHeight: '2' }],
+        // Add more custom font sizes as needed
+      },
+      borderWidth: {
+       25:"25px"
+        
+      },
+      colors: {
+        customBorder: {
+          "red":"#33B3FF"
+        },
+      }
+      
     },
   },
   plugins: [],
